5187f5c970f6a609b9b44aa3adbb9d7fba1c2c4c
Taís Massaro <code@tais.dev>
2021-11-01 22:04:25 +0100

    feat: add higher order component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import styled from 'styled-components'
+import { HigherOrderComponent } from './patterns'
 
 const StyledContainer = styled.div`
   background: #191921;
@@ -8,10 +9,24 @@ const StyledContainer = styled.div`
   min-height: 100vh;
 `
 
+const Box = styled.div`
+  background: #efeff4;
+  border-radius: 16px;
+  width: 240px;
+  height: 240px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 function App () {
   return (
     <StyledContainer>
       <h1>Advanced React Patterns</h1>
+      <h2>Higher Order Component</h2>
+      <Box>
+        <HigherOrderComponent />
+      </Box>
     </StyledContainer>
   )
 }
